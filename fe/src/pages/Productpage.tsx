be10f01065f8e7adf811c686e154a107d7876aae
dangvanphucnghia1 <dangvanphucnghia1@gmail.com>
2025-06-01 00:29:30 +0700

    update

--- fe/src/pages/Productpage.tsx
+++ fe/src/pages/Productpage.tsx
@@ -1,6 +1,6 @@
 import NavBar from "../components/Navbar";
 import React from "react";
-import ProductCard from "../components/Product/productCard";
+import ProductCard from "../components/Product/ProductCard";
 import Section8 from "../components/Homepage/Section8/Section8";
 import Footer from "../components/Footer";
 import ProductService from "../components/Product/ProductService";
--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-// https://vite.dev/config/
+import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  server: {
+    host: '0.0.0.0', // Cho phép truy cập từ domain bên ngoài
+    port: 5177,       // Hoặc cổng khác nếu bạn muốn
+    strictPort: true,
+    origin: 'https://webvibe.wisdombrain.org',
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
   },
 })
